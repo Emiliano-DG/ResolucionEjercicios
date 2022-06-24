2ef21b31eb6a2457f096b824b66428134f73eea1
Emi <95498101+Emiliano-DG@users.noreply.github.com>
2022-06-24 19:40:46 -0300

    Add files via upload

---
+++ Python/Tercera-actividad/tercerActividad.py
@@ -0,0 +1,20 @@
+nombre = 'pepe'
+apellido = 'Gonzalez'
+edad = 27
+email = 'pepeg@gmail'
+telefono = 2383648
+casado = False
+hijos = False
+amigos = ['jorge','matias','gonzalo']
+peliculas = {'clave1':'Interestelar','clave2':'rapido y furioso'}
+
+print(nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(amigos)
+print(peliculas)
+
