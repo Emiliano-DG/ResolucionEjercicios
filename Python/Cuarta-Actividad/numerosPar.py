8feb070bbe587b4365ea81772129bc57991a57d5
Emi <95498101+Emiliano-DG@users.noreply.github.com>
2022-06-27 12:54:47 -0300

    Update numerosPar.py

--- Python/Cuarta-Actividad/numerosPar.py
+++ Python/Cuarta-Actividad/numerosPar.py
@@ -1,6 +1,9 @@
 numeroInicial = 2
 numeroFinal = 8
+numeroImpares = []
 
 for i in range(numeroInicial,numeroFinal):
     if i % 2 != 0:
-        print(i)
+        numeroImpares.append(i)
+
+print(numeroImpares)
