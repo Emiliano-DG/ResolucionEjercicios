e583a89b3161edd6fe2fa86e89c2d44d2aaf4591
Emi <95498101+Emiliano-DG@users.noreply.github.com>
2022-06-28 12:45:16 -0300

    Add files via upload

---
+++ Python/Quinta-Actividad/añoBiciesto.py
@@ -0,0 +1,14 @@
+def Biciesto(dato):
+    if dato % 4 == 0:
+        if dato % 100 == 0:
+            if dato % 400 == 0:
+                print('El año es bisiesto')
+            else:
+             print('El año no es bisiesto')
+        else:
+             print('El año es bisiesto.')
+    else:
+        print('El año no es bisiesto.')
+
+dato = int(input('Ingrese el año: '))
+Biciesto(dato)
