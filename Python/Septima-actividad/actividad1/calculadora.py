32639efcc11cda4fd2c5e1873b43c9f2dfbad888
Emi <95498101+Emiliano-DG@users.noreply.github.com>
2022-07-01 12:44:29 -0300

    Delete Python/Septima-actividad/actividad1 directory

--- Python/Septima-actividad/actividad1/calculadora.py
+++
@@ -1,8 +0,0 @@
-from operaciones import *
-
-a,b = (2,5)
-
-print("{} + {} = {}".format(a,b,suma(a,b)))
-print("{} - {} = {}".format(a,b,resta(a,b)))
-print("{} / {} = {}".format(a,b,division(a,b)))
-print("{} * {} = {}".format(a,b,multiplicar(a,b)))
